7e170009b455a3032fe18d9ec1766ca75c6f209b
Tommaso P <tommaso.plinio@gmail.com>
2021-03-03 16:19:34 +0100

    Prompt

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Lista Cognomi
+// Chiedere all’utente il cognome
+// inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+
+// 1 Chiedere all'utente il cognome
+var cognomeUtente = prompt ("Inserisci il cognome")
